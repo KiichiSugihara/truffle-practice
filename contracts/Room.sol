fc077b8bd837c85b7722aa3e973e436ea23234c5
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-09-01 10:38:55 +0900

    modify(develop): modify roomCon secure

--- contracts/Room.sol
+++ contracts/Room.sol
@@ -2,20 +2,57 @@
 
 pragma solidity ^0.5.10;
 
-import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
 
-contract Room is Ownable{
-  constructor(address _creator) public payable {
-    owner = _creator;
-  }
+contract Room is Destructible, Pausable {
 
-  function deposit() external payable {
-  }
-  function sendReward(uint256 _reward, address _dest) external onlyOwner {
-    _dest.transfer(_reward);
-  }
-  function refundToOwner() external onlyOwner {
-    owner.transfer(address(this).balance);
-  }
+    //状態変数 rewardSent
+    mapping(uint256 => bool) public rewardSent;
 
+    event Deposited(
+        address indexed _depositor,
+        uint256 _depositedValue
+    );
+
+    event RewardSent(
+        address indexed _dest,
+        uint256 _reward,
+        uint256 _id
+    );
+
+    event RefundedToOwner(
+        address indexed _dest,
+        uint256 _refundedBalance
+    );
+
+    constructor(address _creator) public payable {
+        owner = _creator;
+    }
+
+
+    function deposit() external payable whenNotPaused {
+        require(msg.value > 0, "0 Ehter");
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    function sendReward(uint256 _reward, address _dest, uint256 _id) external onlyOwner {
+        require(!rewardSent[_id],"You have Sent");
+        require(_reward > 0,"Need: reward > 0");
+        require(address(this).balance >= _reward,"yours < reward");
+        require(_dest != address(0),"please init address");
+        require(_dest != owner,"address is owner");
+
+        rewardSent[_id] = true;
+        _dest.transfer(_reward);
+        emit RewardSent(_dest, _reward, _id);
+    }
+
+    function refundToOwner() external onlyOwner {
+        require(address(this).balance > 0, "refund is 0");
+
+        uint256 refundedBalance = address(this).balance;
+        owner.transfer(refundedBalance);
+        emit RefundedToOwner(msg.sender, refundedBalance);
+    }
 }
--- contracts/RoomFactory.sol
+++ contracts/RoomFactory.sol
@@ -1,1 +1,13 @@
 // Contract: generate rooms
+
+pragma solidity ^0.5.10;
+
+import "./Room.sol";
+
+contract RoomFactory {
+  function createRoom() external payable {
+    // var constructor = new Room;
+    // address newRoom = constructor.value(msg.value)(msg.sender);
+    address newRoom = (new Room).value(msg.value)(msg.sender);
+  }
+}
